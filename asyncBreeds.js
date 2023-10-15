b7ce359f11666278c471ede68b648fd620583b64
Atkinss1 <jordanatkins@live.com>
2023-10-14 20:30:55 -0600

    fix bug for error if file comes back undefined

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,20 +2,15 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, callback) {
-  console.log('# 1 --->   breedDetailsFromFile: Calling readFile...');
+  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, `utf-8`, (error, data) => {
-    console.log(`# 3 --->   In readFile's Callback: it has the data.`);
+    console.log(`In readFile's Callback: it has the data.`);
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) callback(data);
-
+    if (!error) {
+      callback(data);
+    } else callback(undefined);
   });
   // ISSUE: Attempting to return data out here will also not work..
   //      Currently not returning anything from here, so breedDetailsFromFile function returns undefinded.
 };
-
-
-// we can try to get the return value.
-const bombay = breedDetailsFromFile('Bombay', (data) => {
-  console.log(data);
-});
-console.log('Return value: #2 --->   ', bombay); // => will NOT print out details, instead we will see undefined.
+module.exports = breedDetailsFromFile;
